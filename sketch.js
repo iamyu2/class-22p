2ac025be23da0920b92b6ef13a3186f236eb79bc
iamyu2 <83233218+iamyu2@users.noreply.github.com>
2021-05-16 17:37:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+
+//sprites
+var grid = 50;
+var width = 1366;
+var height = 2700;
+var school;
+var player, cars, logs;
+
+//groups
+var carGroup1, logGroup1, playGruop;
+var grassHeight = 100;
+
+//gamestates
+var gameState = "play";
+
+//image
+var carImage1, carImage2, logImage, logImage2, playerImage;
+
+function preload()
+{
+  
+  //image
+  playerImage = loadImage("Player-03.png"); 
+  carImage1 = loadImage("car1.png");
+  carImage2 = loadImage("car2.png");
+  logImage1 = loadImage("log1.png");
+  logImage2 = loadImage("log2.png");
+   
+}
+
+function setup() 
+{
+
+  //canvas
+  createCanvas(1366, 2700);
+
+  //groups
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+  
+  //road, grass and river
+  for(var i = 0; i < 6; i++)
+  {
+
+    var grass = createSprite(683, 2600 - (i*400), 1366, grassHeight);
+    grass.shapeColor = "gray";
+    if(i%2 === 0)
+    {
+
+      var road = createSprite(683, 2500 - (i*400) - grassHeight, 1366, 300);
+      road.shapeColor = "black";
+
+    }
+
+  }
+
+  //cars
+  for(i = 0; i < 40; i++)
+  {
+
+    cars = new Car(2);
+    carGroup1.add(cars.spt);
+
+  }
+
+  //logs
+  for(i = 0; i < 40; i++)
+  {
+  
+    logs = new Log(2);
+    logGroup1.add(logs.spt);
+  
+  }  
+  
+  //player
+  player = new Player(683, height - 25);
+
+}
+
+function draw() {
+
+  //background
+  background("skyblue");
+
+  //controal for player
+  keyPressed();
+  
+
+  //draw
+  drawSprites();
+
+}
+
+//control
+function keyPressed()
+{
+
+  if(keyCode == UP_ARROW)
+  {player.move(0, -5)}
+  else if(keyCode == DOWN_ARROW)
+  {player.move(0, 5)}
+  else if(keyCode == LEFT_ARROW)
+  {player.move(-5, 0)}
+  else if(keyCode == RIGHT_ARROW)
+  {player.move(5, 0)}
+
+}
+
+
